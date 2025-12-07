e8eb66dd98a91014b8c6f33e9074922f682f8afe
Pravin Bhatkar <pravinbhatkar20034@gmail.com>
2025-12-07 19:03:05 +0530

    id

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ app.get('/ping', (req, res) => {
   res.status(200).send('Pong');
 });
 
-app.use((req, res, next) => {
-  req.user = { _id: '654abc123456789012345678' }; 
-  next();
-});
+
 app.use('/api/v1/cars',carRoutes);
 app.use('/api/v1/trips',tripRoutes);
 app.use('/api/v1/user',userRoutes);
